d128182cd3711aacd52fa4607459fc756c4a2487
vicnon250 <54322956+vicnon250@users.noreply.github.com>
2020-03-11 11:58:48 +0100

    Add files via upload

---
+++ library/CADUsario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace library
+{
+    public class CADUsario
+    {
+        private string constring;
+        public CADUsario() {
+        }
+       /* public bool createUsario(ENUsario en)
+        {
+            return true;
+        }
+        public bool readUsario(ENUsario en)
+        {
+            return true;
+        }
+        public bool readFirstUsario(ENUsario en)
+        {
+            return true;
+        }
+        public bool readNextUsario(ENUsario en)
+        {
+            return true;
+        }
+        public bool readPrevUsario(ENUsario en)
+        {
+            return true;
+        }
+        public bool updateUsario(ENUsario en)
+        {
+            return true;
+        }
+        public bool deleteUsario(ENUsario en)
+        {
+            return true;
+        }*/
+    }
+}
---
+++ library/EnUsario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class ENUsario
+    {
+        private string nif, nombre;
+        private int edad;
+        public ENUsario()
+        {
+
+        }
+        public ENUsario(string Nif,string Nombre,int Edad)
+        {
+            nif = Nif;
+            nombre = Nombre;
+            edad=Edad;
+        }
+        public bool readUsario()
+        {
+            return true;
+        }
+        public bool readFirstUsario()
+        {
+            return true;
+        }
+        public bool readNextUsario()
+        {
+            return true;
+        }
+        public bool readPrevUsario()
+        {
+            return true;
+        }
+        public bool updateUsario()
+        {
+            return true;
+        }
+        public bool deleteUsario()
+        {
+            return true;
+        }
+    }
+}
